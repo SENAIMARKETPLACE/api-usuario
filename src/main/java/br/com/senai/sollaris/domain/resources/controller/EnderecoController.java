73e5392283c742b474685a030c54741200544367
AndrewAscar742 <andrewmatosascar@outlook.com>
2023-03-15 22:35:57 -0300

    Develop: CRUD Address almost ready

--- src/main/java/br/com/senai/sollaris/domain/resources/controller/EnderecoController.java
+++ src/main/java/br/com/senai/sollaris/domain/resources/controller/EnderecoController.java
@@ -1,30 +1,52 @@
 package br.com.senai.sollaris.domain.resources.controller;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import br.com.senai.sollaris.domain.resources.dtos.input.EnderecoDto;
+import br.com.senai.sollaris.domain.resources.dtos.input.PutEnderecoDto;
+import br.com.senai.sollaris.domain.resources.dtos.output.ReturnEnderecoDto;
 import br.com.senai.sollaris.domain.resources.dtos.output.ReturnUsuarioDto;
 import br.com.senai.sollaris.domain.resources.service.EnderecoService;
 
 @RestController
 @RequestMapping("api/user/address")
 public class EnderecoController {
-	
+
 	@Autowired
 	private EnderecoService enderecoService;
 	
+	@GetMapping
+	public List<ReturnEnderecoDto> listarEnderecos(){
+		return enderecoService.listarEnderecos();
+	}
+	
+	@GetMapping("/{id}")
+	public ReturnEnderecoDto listarEndereco(@PathVariable Long id) {
+		return enderecoService.listarEndereco(id);
+	}
+
 	@PostMapping
-	public ResponseEntity<ReturnUsuarioDto> cadastrarEndereco(@RequestBody @Valid EnderecoDto endereco, 
+	public ResponseEntity<ReturnUsuarioDto> cadastrarEndereco(@RequestBody @Valid EnderecoDto endereco,
 			UriComponentsBuilder uriBuilder) {
 		return enderecoService.cadastrarEndereco(endereco, uriBuilder);
-		 
+
+	}
+
+	@PutMapping("/{id}")
+	public void alterarEndereco(@PathVariable Long id, @RequestBody PutEnderecoDto endereco) {
+		enderecoService.alterarEndereco(id, endereco);
 	}
 }
--- src/main/java/br/com/senai/sollaris/domain/resources/service/EnderecoService.java
+++ src/main/java/br/com/senai/sollaris/domain/resources/service/EnderecoService.java
@@ -1,36 +1,56 @@
 package br.com.senai.sollaris.domain.resources.service;
 
 import java.net.URI;
+import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import br.com.senai.sollaris.domain.Endereco;
 import br.com.senai.sollaris.domain.Usuario;
+import br.com.senai.sollaris.domain.repository.EnderecoRepository;
 import br.com.senai.sollaris.domain.resources.dtos.input.EnderecoDto;
+import br.com.senai.sollaris.domain.resources.dtos.input.PutEnderecoDto;
+import br.com.senai.sollaris.domain.resources.dtos.output.ReturnEnderecoDto;
 import br.com.senai.sollaris.domain.resources.dtos.output.ReturnUsuarioDto;
-import lombok.NoArgsConstructor;
+import br.com.senai.sollaris.domain.resources.service.exceptions.ObjetoNaoEncontradoException;
+import lombok.RequiredArgsConstructor;
 
 @Service
-@NoArgsConstructor
+@RequiredArgsConstructor
 public class EnderecoService {
+
+	private final UsuarioService usuarioService;
+	private final EnderecoRepository enderecoRepository;
+
+	public List<ReturnEnderecoDto> listarEnderecos() {
+		return enderecoRepository.findAll().stream().map(ReturnEnderecoDto::new).toList();
+	}
 	
-	@Autowired
-	private UsuarioService usuarioService;
-	
+	public Endereco listarEndereco(Long id) {
+		enderecoRepository.findById(id).orElseThrow(() -> new ObjetoNaoEncontradoException("Endereço não encontrado"));
+		return 
+		
+	}
+
 	@Transactional
-	public ResponseEntity<ReturnUsuarioDto> cadastrarEndereco(EnderecoDto endereco, 
-			UriComponentsBuilder uriBuilder) {
-		//Estou validando o id inserido no Usuario se existe
+	public ResponseEntity<ReturnUsuarioDto> cadastrarEndereco(EnderecoDto endereco, UriComponentsBuilder uriBuilder) {
+		// Estou validando o id inserido no Usuario se existe
 		Usuario usuario = usuarioService.listarUsuario(endereco.getUsuario_id());
 		usuario.adicionarEndereco(usuario, endereco);
-		
-		
+
 		URI uri = uriBuilder.path("api/address/{id}").buildAndExpand(usuario.getId()).toUri();
 		ReturnUsuarioDto usuarioDto = new ReturnUsuarioDto(usuario);
-		
+
 		return ResponseEntity.created(uri).body(usuarioDto);
 	}
+
+	@Transactional
+	public void alterarEndereco(Long id, PutEnderecoDto endereco) {
+
+	}
+
+
 }
