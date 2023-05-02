826d97a31126106167425fc178b978934758f38b
AndrewAscar742 <andrewmatosascar@outlook.com>
2023-05-02 08:56:50 -0300

    refactor: ReturnUser

--- src/main/java/br/com/senai/sollaris/domain/resources/dtos/output/ReturnUsuarioDto.java
+++ src/main/java/br/com/senai/sollaris/domain/resources/dtos/output/ReturnUsuarioDto.java
@@ -19,6 +19,9 @@ public class ReturnUsuarioDto {
 	private Integer id;
 	private LocalDateTime data_requisicao = LocalDateTime.now();
 	private String nome;
+	private String cpf;
+	private String email;
+	private String senha;
 	private Generos genero;
 	private String img;
 	private List<ReturnEnderecoDto> enderecos;
@@ -28,6 +31,9 @@ public class ReturnUsuarioDto {
 		this.nome = usuario.getNome();
 		this.genero = usuario.getGenero();
 		this.img = usuario.getImg();
+		this.cpf = usuario.getCpf();
+		this.email = usuario.getEmail();
+		this.senha = usuario.getSenha();
 		this.enderecos = usuario.getEnderecos().stream().map(endereco -> new ReturnEnderecoDto(endereco)).toList();
 	}
 }
